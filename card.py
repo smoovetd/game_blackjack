50aa4ea799ab5cd6626b7e7f99e28137967535de
smoovetd <smoovetd@gmail.com>
2023-01-04 22:24:20 +0200

    Initial commit with cards, card, blackjack and README.md

---
+++ card.py
@@ -0,0 +1,45 @@
+class Card:
+    def __init__(self, char: str, sign: str) -> None:
+        #self.char = char 
+        #self.sign = sign
+        self.set_char(char) 
+        self.set_sign(sign)
+
+    def set_char(self, char: str) -> None:
+        valid_chars = ['A', 'K', 'Q' ,'J', '10', '9', '8', '7', '6', '5', '4', '3', '2']
+        if not char in valid_chars:
+            print(f'ERROR: incorrect character: {char}. Valid values: {valid_chars}')        
+            self.char = 'ERROR_CHAR'
+        else:
+            self.char = char
+
+    def get_char(self) -> str:
+        return self.char
+
+    def set_sign(self, sign) -> None:
+        valid_signs = {'spades': '\u2660', 'hearts': '\u2665', 'diamonds': '\u2666', 'cups': '\u2663'}
+        if not sign in valid_signs.keys():
+            print(f'ERROR: incorrect sign: {sign}. Expected signs are: {valid_signs.keys()}')
+            self.sign = 'ERROR_SIGN'
+        else:
+
+            self.sign = valid_signs[sign]
+
+    def get_sign(self):
+        return self.sign
+
+    def __str__(self) -> str:
+        print(f'DEBUG INFO: char: {self.char} sign: {self.sign}')
+        if self.char == '10':
+            draw_card = f' ___\n|{self.get_char()} |\n| {self.get_sign()} |\n|_{self.get_char()}|\n'
+        else: 
+            draw_card = f' ___\n|{self.get_char()}  |\n| {self.get_sign()} |\n|__{self.get_char()}|\n'
+
+        return draw_card
+
+
+
+#card = Card('Q', 'hearts')
+#print(str(card))
+#print(card.sign)
+print(1120/2)
---
+++ deck.py
@@ -0,0 +1,79 @@
+from typing import final
+from card import Card
+import random
+
+class Deck:
+    def __init__(self) -> None:
+        self.cards = self.get_full_deck()
+
+        self.used_cards = list()
+
+    def get_all_cards(self) -> tuple:
+        return self.cards
+
+    def draw_card(self) -> Card: 
+        crnt_card = None
+        while (crnt_card = random.choice(self.cards)):
+            
+
+
+    def get_full_deck(self) -> tuple:
+        all_cards =  (Card ('A', 'spades'), Card ('2', 'spades'), Card ('3', 'spades'), Card ('4', 'spades'), \
+        Card ('5', 'spades'), Card ('6', 'spades'), Card ('7', 'spades'), Card ('8', 'spades'), \
+        Card ('9', 'spades'), Card ('10', 'spades'), Card ('J', 'spades'), Card ('Q', 'spades'), \
+        Card ('K', 'spades'), Card ('A', 'hearts'), Card ('2', 'hearts'), Card ('3', 'hearts'), \
+        Card ('4', 'hearts'), Card ('5', 'hearts'), Card ('6', 'hearts'), Card ('7', 'hearts'), \
+        Card ('8', 'hearts'), Card ('9', 'hearts'), Card ('10', 'hearts'), Card ('J', 'hearts'), \
+        Card ('Q', 'hearts'), Card ('K', 'hearts'), Card ('A', 'diamonds'), Card ('2', 'diamonds'), \
+        Card ('3', 'diamonds'), Card ('4', 'diamonds'), Card ('5', 'diamonds'), Card ('6', 'diamonds'), \
+        Card ('7', 'diamonds'), Card ('8', 'diamonds'), Card ('9', 'diamonds'), Card ('10', 'diamonds'), \
+        Card ('J', 'diamonds'), Card ('Q', 'diamonds'), Card ('K', 'diamonds'), Card ('A', 'cups'), \
+        Card ('2', 'cups'), Card ('3', 'cups'), Card ('4', 'cups'), Card ('5', 'cups'), \
+        Card ('6', 'cups'), Card ('7', 'cups'), Card ('8', 'cups'), Card ('9', 'cups'), \
+        Card ('10', 'cups'), Card ('J', 'cups'), Card ('Q', 'cups'), Card ('K', 'cups'))
+
+        return all_cards
+
+    def print_all_cards(self) -> None:
+        all_cards = ''
+        for card in self.cards:
+            all_cards += str(card) + '\n'
+
+        print(all_cards)
+
+    def print_list_cards(self, cards: list, cards_per_row:int = 4) -> None:
+        cards_to_print = ''
+        crnt_count = 0
+        for card in cards:
+            cards_to_print += str(card)
+
+        crnt_cards = cards_to_print.split('\n')
+        #print(crnt_cards)
+        cards_list = list()
+        final_str = ''
+        count = 0
+        for card_part in range (0,4):
+            for card in range(card_part, len(crnt_cards) -1, 4):
+                #print(f'part: {card_part}; card: {card}; current part of card: {crnt_cards[card]}')
+
+                if card_part == 0 and card !=0:
+                    final_str += ' '
+                final_str += ' '
+
+                final_str += crnt_cards[card]
+                
+            #print(f'DEBUG: {final_str}')   
+            final_str = final_str + '\n'
+            
+            
+        print(final_str)
+
+
+deck = Deck()
+#deck.print_all_cards()
+cards_to_return = list()
+cards_to_return.append(deck.get_all_cards()[0])
+cards_to_return.append(deck.get_all_cards()[1])
+cards_to_return.append(deck.get_all_cards()[2])
+cards_to_return.append(deck.get_all_cards()[10])
+deck.print_list_cards(cards_to_return)
