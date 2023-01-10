857fca060bd0ca416c6e65709ec49b0ea9f0e168
smoovetd <smoovetd@gmail.com>
2023-01-10 23:22:56 +0200

    Added two more tests in test_decks.py

--- tests/test_decks.py
+++ tests/test_decks.py
@@ -1,5 +1,6 @@
 import sys 
 import os 
+import random
 
 # inserting parent directory
 sys.path.insert(0, os.getcwd())
@@ -23,3 +24,27 @@ def test_draw_card():
     assert len(deck.unused_cards) == deck_length - 1, f'Incorrect length of unused cards after draw! Expected {deck_length - 1}, actual {len(deck.unused_cards)}'
     assert deck.used_cards[0] == card, f'Expected card to be {card}, Actual: {deck.used_cards[0]}' 
     assert (card in deck.unused_cards) == False,  f'Error - card is found in unused_cards list!'
+
+
+def test_draw_cards():
+    deck_length = 52
+    number_of_cards = random.randint(1, deck_length)
+    deck = Deck()
+    crnt_cards = deck.draw_cards(number_of_cards)
+
+    assert len(deck.used_cards) == number_of_cards, f'Incorrect number of used cards! Expected: {number_of_cards}, Actual: {len(deck.used_cards)}!' 
+    assert len(crnt_cards) == number_of_cards, f'Incorrect number of drawn cards! Expected: {number_of_cards}, Actual: {len(crnt_cards)}!'
+    assert len(deck.unused_cards) == (deck_length - number_of_cards), f'Incorrect number of unused cards! Expected: {deck_length - number_of_cards}, Actual: {len(deck.unused_cards)}!'
+
+    for card in crnt_cards:
+        assert card in deck.used_cards, f'Card is not in used cards!'
+        assert card not in deck.unused_cards, f'Card is in unused cards!'
+
+
+def test_draw_card_no_cards_left():
+    deck_length = 52
+    deck = Deck()
+    all_cards = deck.draw_cards(deck_length)
+
+    assert deck.draw_card() == None, f'Card is not None after all cards are drawn'
+    assert deck.draw_card() == None, f'Card is not None after all cards are drawn - second attempt'
